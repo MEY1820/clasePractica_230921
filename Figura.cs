0766262a0091d19d16b22c2d22cc131d7927daf5
MEY1820 <87822323+MEY1820@users.noreply.github.com>
2021-09-26 22:10:04 -0600

    Add files via upload

---
+++ Figura.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_230921
+{
+    class Figura
+    {
+        // Definir Miembros
+        // Propiedades 
+
+        public string _figuraId { get; set; }
+        public string _forma { get; set; }
+        public string _angulo { get; set; }
+
+        // Propiedades autoimplementada
+        public string _lados { get; set; }
+
+        // Metodo constructor
+        public Figura(string figuraId, string forma, string angulo, string lados) // Metodo constructor vacio
+        {
+            _figuraId = figuraId;
+            _forma = forma;
+            _angulo = angulo;
+            _lados = lados;
+
+        }
+        public string ObtenerDatos()
+        {
+            return "Numero de la figura: " + _figuraId + " Forma: " + _forma + " Cantidad de angulos: "
+                + _angulo + " Cantidad de lados: " + _lados;
+
+        }
+    }
+
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace clase_230921
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.tbFigura = new System.Windows.Forms.TextBox();
+            this.tbAngulos = new System.Windows.Forms.TextBox();
+            this.tbForma = new System.Windows.Forms.TextBox();
+            this.tbLados = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbDatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.MistyRose;
+            this.btnAgregar.Font = new System.Drawing.Font("Gabriola", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAgregar.Location = new System.Drawing.Point(263, 40);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(257, 43);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Ingresar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // tbFigura
+            // 
+            this.tbFigura.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tbFigura.Location = new System.Drawing.Point(87, 108);
+            this.tbFigura.Name = "tbFigura";
+            this.tbFigura.Size = new System.Drawing.Size(160, 20);
+            this.tbFigura.TabIndex = 1;
+            // 
+            // tbAngulos
+            // 
+            this.tbAngulos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tbAngulos.Location = new System.Drawing.Point(87, 186);
+            this.tbAngulos.Name = "tbAngulos";
+            this.tbAngulos.Size = new System.Drawing.Size(160, 20);
+            this.tbAngulos.TabIndex = 2;
+            // 
+            // tbForma
+            // 
+            this.tbForma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tbForma.Location = new System.Drawing.Point(517, 108);
+            this.tbForma.Name = "tbForma";
+            this.tbForma.Size = new System.Drawing.Size(146, 20);
+            this.tbForma.TabIndex = 3;
+            // 
+            // tbLados
+            // 
+            this.tbLados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.tbLados.Location = new System.Drawing.Point(517, 186);
+            this.tbLados.Name = "tbLados";
+            this.tbLados.Size = new System.Drawing.Size(146, 20);
+            this.tbLados.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(84, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Numero de la figura:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(542, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Forma:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(84, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cantidad de angulos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(514, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cantidad de lados:";
+            // 
+            // lbDatos
+            // 
+            this.lbDatos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lbDatos.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.lbDatos.FormattingEnabled = true;
+            this.lbDatos.Location = new System.Drawing.Point(145, 243);
+            this.lbDatos.Name = "lbDatos";
+            this.lbDatos.Size = new System.Drawing.Size(443, 134);
+            this.lbDatos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbDatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLados);
+            this.Controls.Add(this.tbForma);
+            this.Controls.Add(this.tbAngulos);
+            this.Controls.Add(this.tbFigura);
+            this.Controls.Add(this.btnAgregar);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox tbFigura;
+        private System.Windows.Forms.TextBox tbAngulos;
+        private System.Windows.Forms.TextBox tbForma;
+        private System.Windows.Forms.TextBox tbLados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbDatos;
+    }
+}
+
